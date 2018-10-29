e92aad1261bda6adc2463fd227c46fa3f242d144
Sylvia Liu <sylvia.liu@omnicell.com>
2018-10-29 13:40:39 -0700

    modify controller

--- CS/Server/Server/Controllers/ReportDesignerController.cs
+++ CS/Server/Server/Controllers/ReportDesignerController.cs
@@ -24,22 +24,12 @@ namespace Server.Controllers {
         public ActionResult GetReportDesignerModel(string reportUrl) {
             Response.AppendHeader("Access-Control-Allow-Origin", "*");
 
-            //string modelJsonScript =
-            //    new ReportDesignerClientSideModelGenerator()
-            //    .GetJsonModelScript(
-            //        reportUrl,                 // Obtain a report to open in the Report Designer on application start.
-            //        GetAvailableDataSources(), // Obtain data sources that will be available in the Report Designer for adding to reports.
-            //        "ReportDesigner/Invoke",   // The URI path of the controller action that processes requests from the Report Designer.
-            //        "WebDocumentViewer/Invoke",// The URI path of the controller action that processes requests from the Web Document Viewer.
-            //        "QueryBuilder/Invoke"      // The URI path of the controller action that processes requests from the Query Builder.
-            //    );
-
             string modelJsonScript = new ReportDesignerClientSideModelGenerator().GetJsonModelScript(
                     reportUrl,
-                    null,
+                    null, // GetAvailableDataSources(), // Obtain data sources that will be available in the Report Designer for adding to reports.
                     "ReportDesigner/Invoke",
-                    null,
-                    null
+                    "WebDocumentViewer/Invoke",
+                    "QueryBuilder/Invoke"
                 );
             return Content(modelJsonScript, "application/json");
         }
