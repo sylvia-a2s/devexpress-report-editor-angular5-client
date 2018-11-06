3759f4769b9d29e80ff563c5fe71420c368e53e4
Sylvia Liu <sylvia.liu@omnicell.com>
2018-11-05 21:56:14 -0800

    return a list of report template

--- JS/ClientSide_Angular5+/src/app/report-designer/report-designer.component.ts
+++ JS/ClientSide_Angular5+/src/app/report-designer/report-designer.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, AfterViewInit, ElementRef, Renderer2, Input } from '@angular/core';
 import * as ko from 'knockout';
-import { Html } from 'devexpress-reporting/dx-report-designer';
+import { Html, ReportDesigner } from 'devexpress-reporting/dx-report-designer';
+import { calcBindingFlags } from '@angular/core/src/view/util';
 
 @Component({
   selector: 'report-designer',
@@ -9,9 +10,12 @@ import { Html } from 'devexpress-reporting/dx-report-designer';
 
 export class ReportDesignerComponent implements AfterViewInit {
   koReportUrl = ko.observable(null);
+  // urls = ko.observableArray([]);
   _reportUrl;
   designerOptions;
   testnum = 3;
+  currentNum;
+  public listUrls = 'test';
   constructor(private renderer: Renderer2) { }
 
   @ViewChild('scripts')
@@ -31,6 +35,22 @@ export class ReportDesignerComponent implements AfterViewInit {
       requestOptions: { // Options for processing requests from the Report Designer.
         host, // URI of your backend project.
         getDesignerModelAction: '/ReportDesigner/GetReportDesignerModel' // Action that returns the Report Designer model.
+     },
+     callbacks: {
+       ReportOpened: function(s, e) {
+         this.currentNum = e.Url;
+       },
+       CustomizeOpenDialog: function(s, e) {
+        // const urls = ko.observableArray([]);
+        s.ReportStorageGetUrls().done(function (result) {
+            const test = result;
+            console.log(test);
+        });
+        // console.log(urls.map(res => res));
+       }
+      //  ReportSaved: function(s, e) {
+      //     s.CloseCurrentTab();
+      //  }
      }
     };
     ko.applyBindings(this.designerOptions, this.control.nativeElement);
